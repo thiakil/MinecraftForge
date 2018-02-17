63eb7fd6db64db6e2f3d6b2f7bf5644bd1f8eca0
Xander <x@xandervictory.id.au>
2018-02-17 13:47:04 +0800

    let ForgeTestRunner work in J9

--- src/test/java/net/minecraftforge/fml/common/registry/ForgeTestRunner.java
+++ src/test/java/net/minecraftforge/fml/common/registry/ForgeTestRunner.java
@@ -1,6 +1,8 @@
 package net.minecraftforge.fml.common.registry;
 
 import com.google.common.collect.Sets;
+import net.minecraft.launchwrapper.LogWrapper;
+import org.apache.logging.log4j.Level;
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.Failure;
@@ -8,9 +10,13 @@ import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.JUnit4;
 import org.junit.runners.model.InitializationError;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLClassLoader;
 import java.util.Collections;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 /**
  * Uses {@code ResettingClassLoader} to load the test class. Minecraft and Forge
@@ -89,7 +95,7 @@ public class ForgeTestRunner extends Runner
      * If a class name starts with any of the supplied patterns, it is loaded by
      * <em>this</em> classloader; otherwise it is loaded by the parent classloader.
      */
-    private class ResettingClassLoader extends URLClassLoader
+    private static class ResettingClassLoader extends URLClassLoader
     {
         private final Set<String> quarantinedClassNames;
 
@@ -98,7 +104,7 @@ public class ForgeTestRunner extends Runner
          */
         public ResettingClassLoader(String... quarantinedClassNames)
         {
-            super(((URLClassLoader) getSystemClassLoader()).getURLs());
+            super(getClassloaderURLs());
 
             this.quarantinedClassNames = Sets.newHashSet();
             Collections.addAll(this.quarantinedClassNames, quarantinedClassNames);
@@ -144,6 +150,55 @@ public class ForgeTestRunner extends Runner
             }
 
             return super.loadClass(name);
+        }
+
+        private static URL[] getClassloaderURLs(){
+            URL[] urls;
+            if (ResettingClassLoader.class.getClassLoader() instanceof URLClassLoader){
+                urls = ((URLClassLoader) ResettingClassLoader.class.getClassLoader()).getURLs();
+            } else {
+                String classPath = appendPath(System.getProperty("java.class.path"), System.getProperty("env.class.path"));
+                urls = pathToURLs(classPath);
+            }
+            return urls;
+        }
+
+        private static URL fileToURL(File file) {
+            try {
+                return file.toURI().toURL();
+            } catch (MalformedURLException e) {
+                LogWrapper.log(Level.ERROR, e, "Could not convert {} to URL", file.toString());
+                return null;
+            }
+        }
+
+        //the following 2 methods are largely copied from internal sun.* classes
+        public static String appendPath(String pathTo, String pathFrom) {
+            if (pathTo == null || pathTo.length() == 0) {
+                return pathFrom;
+            } else if (pathFrom == null || pathFrom.length() == 0) {
+                return pathTo;
+            } else {
+                return pathTo  + File.pathSeparator + pathFrom;
+            }
+        }
+
+        public static URL[] pathToURLs(String path) {
+            StringTokenizer st = new StringTokenizer(path, File.pathSeparator);
+            URL[] urls = new URL[st.countTokens()];
+            int count = 0;
+            while (st.hasMoreTokens()) {
+                URL url = fileToURL(new File(st.nextToken()));
+                if (url != null) {
+                    urls[count++] = url;
+                }
+            }
+            if (urls.length != count) {
+                URL[] tmp = new URL[count];
+                System.arraycopy(urls, 0, tmp, 0, count);
+                urls = tmp;
+            }
+            return urls;
         }
     }
 }
